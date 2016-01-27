ca483063f24bc706186ab63835d44622b4505444
yasminpaz <pazyasminl@gmail.com>
2016-01-27 06:22:49 -0300

    .cpp e .h foram criados com alguns atributos e métodos básicos
    Obs: sujeito a futuras mudanças e correções.

---
+++ Xbox360/Xbox360.h
@@ -0,0 +1,20 @@
+#ifndef XBOX360_H
+#define XBOX360_H
+
+class Xbox360
+{
+    double versao;
+    double hd;
+    int coord_xyz[3];
+    bool onoff;
+    bool kinect_onoff;
+public:
+    Xbox360();
+    ~Xbox360();
+    void power_on_off();
+    void kinect_on_off();
+    void instalar_jogo();
+    void desinstalar_jogo();
+};
+
+#endif // XBOX360_H
