e97d4027dcc1b85b2c60f085760b9332c8639bfc
yasminpaz <pazyasminl@gmail.com>
2016-02-24 16:00:40 -0300

    Main incompleta (menu teve de ser alterado quando mudei a organização do projeto).

--- main.cpp
+++ main.cpp
@@ -1,15 +1,21 @@
-#include <stdio.h>
+/*  1ª atividade de Programação II
+ *  Autora: Yasmin Paz
+ *  
+ */
 #include <iostream>
+#include <string>
+#include "Console.h"
+#include "Date.h"
+#include "Xbox360"
 using std::cout;
-
-#include "Xbox360.h"
+using std::cin;
 
 int main(int argc, char **argv)
 {
-	Xbox360 Xbox;
-    	Xbox.powerOnOff();
-
-    Xbox.displayInfo();
-    Xbox.displayUsers();
+    Date release (22, 11, 2005);
+	Console myConsole ( true, 0, Microsoft, release, "XBX360.0.0.1");
+    Xbox360 myXbox (true, "Xbox360");
+    
+    
     return 0;
 }
