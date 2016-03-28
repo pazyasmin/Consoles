a13e8a3bdd6bb106e1c73f7254c3b712d75750d7
Yasmin <paz.yasminl@gmail.com>
2016-03-28 15:51:57 -0300

    Update Device.h

--- ProjetoCpp/Device.h
+++ ProjetoCpp/Device.h
@@ -16,11 +16,11 @@ class Device
         void insertStorageDevice(const float &, int);       //Insere dispositivos de armazenamento externo
         void removeStorageDevice(int);                      //Remove dispositivos de armazenamento externo
         void deviceInfo() const;                            //Exibe as informações de dispositivo 
-        void power_ON();                                    //Ligar
-        void power_OFF();                                   //Desligar
+        virtual void power_ON() = 0;                        //Ligar
+        virtual void power_OFF() = 0;                       //Desligar
         void deviceInfo();
-    
-        virtual void start() = 0; 
+        const Device &operator=(const Device &);            //Operador de atribuição
+        bool operator==(const Device &) const;              //Operador de igualdade
         
     protected:
         const static int USB_PORTS = 2;                     //Máximo disponível de entradas USB
@@ -31,7 +31,6 @@ class Device
         float extStorage[USB_PORTS];                        //Armazenamento externo (HDs externos, flash drives, etc)
 
 };
-
 #endif //DEVICE_H
 
 
